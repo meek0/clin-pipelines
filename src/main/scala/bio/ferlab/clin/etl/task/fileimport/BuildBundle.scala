9b8d3391c1c2f905db985bba91bfff824781e23c
jecos <jcostanza@ferlab.bio>
2021-06-02 15:07:53 -0400

    refactor: CLIN-12 Rename and move files

--- src/main/scala/bio/ferlab/clin/etl/task/fileimport/BuildBundle.scala
+++ src/main/scala/bio/ferlab/clin/etl/task/fileimport/BuildBundle.scala
@@ -1,26 +1,26 @@
-package bio.ferlab.clin.etl.task
+package bio.ferlab.clin.etl.task.fileimport
 
 import bio.ferlab.clin.etl.ValidationResult
-import bio.ferlab.clin.etl.fhir.FhirUtils.{EitherResourceExtension, IdTypeExtension, ResourceExtension}
+import bio.ferlab.clin.etl.conf.FerloadConf
 import bio.ferlab.clin.etl.fhir.IClinFhirClient
-import bio.ferlab.clin.etl.model._
-import bio.ferlab.clin.etl.task.validation.DocumentReferencesValidation.validateFiles
-import bio.ferlab.clin.etl.task.validation.OrganizationValidation.validateOrganization
-import bio.ferlab.clin.etl.task.validation.PatientValidation.validatePatient
-import bio.ferlab.clin.etl.task.validation.ServiceRequestValidation.validateServiceRequest
-import bio.ferlab.clin.etl.task.validation.SpecimenValidation.{validateSample, validateSpecimen}
-import bio.ferlab.clin.etl.task.validation.TaskExtensionValidation
+import bio.ferlab.clin.etl.task.fileimport.model._
+import bio.ferlab.clin.etl.task.fileimport.validation.DocumentReferencesValidation.validateFiles
+import bio.ferlab.clin.etl.task.fileimport.validation.OrganizationValidation.validateOrganization
+import bio.ferlab.clin.etl.task.fileimport.validation.PatientValidation.validatePatient
+import bio.ferlab.clin.etl.task.fileimport.validation.ServiceRequestValidation.validateServiceRequest
+import bio.ferlab.clin.etl.task.fileimport.validation.SpecimenValidation.{validateSample, validateSpecimen}
+import bio.ferlab.clin.etl.task.fileimport.validation.TaskExtensionValidation._
 import ca.uhn.fhir.rest.client.api.IGenericClient
 import cats.data.ValidatedNel
-import cats.implicits._
 import org.hl7.fhir.r4.model.Bundle.BundleEntryComponent
 import org.hl7.fhir.r4.model.IdType
-
+import bio.ferlab.clin.etl.fhir.FhirUtils._
+import cats.implicits._
 object BuildBundle {
 
   def validate(metadata: Metadata, files: Seq[FileEntry])(implicit clinClient: IClinFhirClient, fhirClient: IGenericClient, ferloadConf: FerloadConf): ValidationResult[TBundle] = {
     println("################# Validate Resources ##################")
-    val taskExtensions = TaskExtensionValidation.validateTaskExtension(metadata)
+    val taskExtensions = validateTaskExtension(metadata)
     val mapFiles = files.map(f => (f.filename, f)).toMap
     val allResources: ValidatedNel[String, List[BundleEntryComponent]] = metadata.analyses.toList.map { a =>
 
