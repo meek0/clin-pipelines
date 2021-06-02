d88aa27f40fae938848580b62f07693cad0ccd4a
jecos <jcostanza@ferlab.bio>
2021-06-02 14:33:39 -0400

    fix: CLIN-12 Use new S3 libraries in exporter

--- src/main/scala/bio/ferlab/clin/etl/task/ingestion/Exporter.scala
+++ src/main/scala/bio/ferlab/clin/etl/task/ingestion/Exporter.scala
@@ -1,28 +1,33 @@
 package bio.ferlab.clin.etl.task.ingestion
 
+import bio.ferlab.clin.etl.task.Conf
+import cats.data.Validated.Invalid
+import cats.data.{NonEmptyList, Validated}
+
 import scala.concurrent.duration.DurationInt
 
 object Exporter extends App {
 
-  val Array(tables, keyCloakSecret, storeAccess, storeSecret) = args
+  val result: Validated[NonEmptyList[String], Unit] = Conf.readConf().map { conf =>
+    val Array(tables) = args
+    val fhirExporter = new FhirBulkExporter(
+      conf.keycloak, conf.fhir.url, conf.aws
+    )
 
-  case class Config(url: String, id: String, secret: String)
+    val url = tables match {
+      case "all" => fhirExporter.requestBulkExportFor(FhirBulkExporter.ALL_ENTITIES)
+      case entities => fhirExporter.requestBulkExportFor(entities)
+    }
 
-  val keyCloakConfig = Config("https://auth.qa.cqdg.ferlab.bio/auth/realms/clin/protocol/openid-connect/token", "clin-system", keyCloakSecret)
-  val storeConfig = Config("https://esc.calculquebec.ca:8080", storeAccess, storeSecret)
-  val fhirExporter = new FhirBulkExporter(
-    authConfig = keyCloakConfig,
-    fhirUrl= "https://fhir.qa.clin.ferlab.bio/fhir",
-    storeConfig = storeConfig
-  )
-
-  val url = tables match {
-    case "all" => fhirExporter.requestBulkExportFor(FhirBulkExporter.ALL_ENTITIES)
-    case entities => fhirExporter.requestBulkExportFor(entities)
+    val files = fhirExporter.checkPollingStatus(url, 1.seconds, 100.seconds)
+    fhirExporter.uploadFiles(files)
   }
-
-  val files = fhirExporter.checkPollingStatus(url, 1.seconds, 100.seconds)
-  fhirExporter.uploadFiles(files)
-
+  result match {
+    case Invalid(NonEmptyList(h, t)) =>
+      println(h)
+      t.foreach(println)
+      System.exit(-1)
+    case Validated.Valid(_) => println("Success!")
+  }
 }
 
