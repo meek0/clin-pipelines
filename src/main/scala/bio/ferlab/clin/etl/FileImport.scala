d77c530d60d0cbd7fae01f5cf78a960d5a5ce4c5
jecos <jcostanza@ferlab.bio>
2021-08-03 14:02:47 -0400

    feat: CLIN-12 Manage dry run test

--- src/main/scala/bio/ferlab/clin/etl/FileImport.scala
+++ src/main/scala/bio/ferlab/clin/etl/FileImport.scala
@@ -15,29 +15,40 @@ import software.amazon.awssdk.services.s3.S3Client
 object FileImport extends App {
 
   withConf { conf =>
-    val Array(bucket, prefix, bucketDest, prefixDest) = args
+    val (bucket, prefix, bucketDest, prefixDest, dryRun) = args match {
+      case Array(b, p, bd, pd) => (b, p, bd, pd, false)
+      case Array(b, p, bd, pd, "true") => (b, p, bd, pd, true)
+      case Array(b, p, bd, pd, "false") => (b, p, bd, pd, false)
+    }
     val s3Client: S3Client = buildS3Client(conf.aws)
     val (clinClient, client) = buildFhirClients(conf.fhir, conf.keycloak)
-    run(bucket, prefix, bucketDest, prefixDest)(s3Client, client, clinClient, conf.ferload)
+    run(bucket, prefix, bucketDest, prefixDest, dryRun)(s3Client, client, clinClient, conf.ferload)
   }
 
 
-  def run(inputBucket: String, inputPrefix: String, outputBucket: String, outputPrefix: String)(implicit s3: S3Client, client: IGenericClient, clinFhirClient: IClinFhirClient, ferloadConf: FerloadConf) = {
+  def run(inputBucket: String, inputPrefix: String, outputBucket: String, outputPrefix: String, dryRun: Boolean = false)(implicit s3: S3Client, client: IGenericClient, clinFhirClient: IClinFhirClient, ferloadConf: FerloadConf) = {
     val metadata: ValidatedNel[String, Metadata] = Metadata.validateMetadataFile(inputBucket, inputPrefix)
     metadata.andThen { m: Metadata =>
       val rawFileEntries = CheckS3Data.loadRawFileEntries(inputBucket, inputPrefix)
       val fileEntries = CheckS3Data.loadFileEntries(m, rawFileEntries)
-      (BuildBundle.validate(m, fileEntries), CheckS3Data.validateFileEntries(rawFileEntries, fileEntries))
-        .mapN { (bundle, files) =>
-          try {
-            CheckS3Data.copyFiles(files, outputBucket, outputPrefix)
-            bundle.save()
-          } catch {
-            case e: Exception =>
-              CheckS3Data.revert(files, outputBucket, outputPrefix)
-              throw e
+      val results = (BuildBundle.validate(m, fileEntries), CheckS3Data.validateFileEntries(rawFileEntries, fileEntries))
+      if (!dryRun) {
+        results
+          .mapN { (bundle, files) =>
+            try {
+
+              CheckS3Data.copyFiles(files, outputBucket, outputPrefix)
+              bundle.save()
+            } catch {
+              case e: Exception =>
+                CheckS3Data.revert(files, outputBucket, outputPrefix)
+                throw e
+            }
           }
-        }
+      } else {
+        results
+          .mapN { (bundle, files) => (bundle, files) }
+      }
     }
   }
 }
