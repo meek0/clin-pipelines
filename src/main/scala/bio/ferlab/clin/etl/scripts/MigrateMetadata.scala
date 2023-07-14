d5b9f54a47284d078ad294067ce0e74a67d583d5
jecos <jcostanza@ferlab.bio>
2023-07-14 14:38:48 -0400

    fix: CLIN-2056 Add log into MigrateMetadata

--- src/main/scala/bio/ferlab/clin/etl/scripts/MigrateMetadata.scala
+++ src/main/scala/bio/ferlab/clin/etl/scripts/MigrateMetadata.scala
@@ -1,15 +1,19 @@
 package bio.ferlab.clin.etl.scripts
 
 import bio.ferlab.clin.etl.conf.Conf
+import bio.ferlab.clin.etl.ValidationResult
 import bio.ferlab.clin.etl.s3.S3Utils
 import bio.ferlab.clin.etl.s3.S3Utils.buildS3Client
 import bio.ferlab.clin.etl.task.fileimport.model.OldMetadata
 import cats.data.{NonEmptyList, Validated, ValidatedNel}
 import cats.implicits.catsSyntaxValidatedId
+import org.slf4j.{Logger, LoggerFactory}
 import play.api.libs.json.Json
 import software.amazon.awssdk.services.s3.S3Client
 
 object MigrateMetadata {
+
+  val LOGGER: Logger = LoggerFactory.getLogger(getClass)
 
   def apply(prefix: String, conf: Conf): ValidatedNel[String, String] = {
     implicit val s3Client: S3Client = buildS3Client(conf.aws)
@@ -17,12 +21,15 @@ object MigrateMetadata {
     run(bucket, prefix)
   }
 
-  def run(bucket: String, prefix: String)(implicit s3Client: S3Client): ValidatedNel[String, String] = {
+  def run(bucket: String, prefix: String)(implicit s3Client: S3Client): ValidationResult[String] = {
     val oldMetadata: ValidatedNel[String, OldMetadata] = OldMetadata.validateMetadataFile(bucket, prefix)
     oldMetadata.andThen { m: OldMetadata =>
+      LOGGER.info("Old metadata is valid, convert it to new metadata")
       val newMetadata = m.toNewMetadata
       val json = Json.prettyPrint(Json.toJson(newMetadata))
-      S3Utils.writeContent(bucket, s"$prefix/metadata.json", json)
+      val s3Path = s"$prefix/metadata.json"
+      LOGGER.info(s"Write content into bucker $bucket at path $s3Path with content: $json")
+      S3Utils.writeContent(bucket, s3Path, json)
       json.validNel[String]
     }
   }
