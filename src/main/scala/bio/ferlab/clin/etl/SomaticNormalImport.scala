14bd027bd6103fba898af15cc0a7bdfc567e66ae
Yann MARCOU <creativeyann17@gmail.com>
2024-03-07 17:59:27 -0500

    feat: CLIN-2528 s3 copy + docref + task wip

--- src/main/scala/bio/ferlab/clin/etl/SomaticNormalImport.scala
+++ src/main/scala/bio/ferlab/clin/etl/SomaticNormalImport.scala
@@ -1,24 +1,31 @@
 package bio.ferlab.clin.etl
 
-import bio.ferlab.clin.etl.Scripts.args
-import bio.ferlab.clin.etl.conf.Conf
+import bio.ferlab.clin.etl.conf.{AWSConf, Conf}
 import bio.ferlab.clin.etl.fhir.FhirClient.buildFhirClients
-import bio.ferlab.clin.etl.fhir.IClinFhirClient
+import bio.ferlab.clin.etl.fhir.FhirUtils
+import bio.ferlab.clin.etl.fhir.FhirUtils.Constants.CodingSystems.{ANALYSIS_TYPE, DR_CATEGORY, DR_FORMAT, DR_TYPE}
+import bio.ferlab.clin.etl.fhir.FhirUtils.Constants.Extensions.{FULL_SIZE, SEQUENCING_EXPERIMENT}
 import bio.ferlab.clin.etl.s3.S3Utils.buildS3Client
 import bio.ferlab.clin.etl.scripts.SwitchSpecimenValues
 import bio.ferlab.clin.etl.task.fileimport.CheckS3Data
+import bio.ferlab.clin.etl.task.fileimport.CheckS3Data.attach
+import bio.ferlab.clin.etl.task.fileimport.model.{FileEntry, RawFileEntry, TBundle}
 import bio.ferlab.clin.etl.task.fileimport.model.TTask.{EXOME_GERMLINE_ANALYSIS, EXTUM_ANALYSIS}
 import ca.uhn.fhir.rest.client.api.IGenericClient
+import cats.data.Validated.Valid
 import org.apache.commons.io.IOUtils
-import org.hl7.fhir.r4.model.Bundle.SearchEntryMode
+import org.apache.http.entity.ContentType.APPLICATION_OCTET_STREAM
+import org.hl7.fhir.r4.model.Bundle.{BundleEntryComponent, SearchEntryMode}
 import org.hl7.fhir.r4.model.Enumerations.DocumentReferenceStatus
-import org.hl7.fhir.r4.model.{Bundle, CodeableConcept, Coding, DocumentReference, Task}
+import org.hl7.fhir.r4.model.Task.ParameterComponent
+import org.hl7.fhir.r4.model._
 import org.slf4j.{Logger, LoggerFactory}
 import software.amazon.awssdk.services.s3.S3Client
-import software.amazon.awssdk.services.s3.model.{GetObjectRequest, ListObjectsRequest}
+import software.amazon.awssdk.services.s3.model.GetObjectRequest
 
-import java.util.Scanner
+import java.util.{Scanner, UUID}
 import java.util.zip.GZIPInputStream
+import scala.Seq
 import scala.collection.JavaConverters.asScalaBufferConverter
 
 object SomaticNormalImport extends App {
@@ -41,17 +48,44 @@ object SomaticNormalImport extends App {
     implicit val s3Client = buildS3Client(conf.aws)
     val (_, fhirClient) = buildFhirClients(conf.fhir, conf.keycloak)
     val bucket = conf.aws.bucketName
+    val bucketOutputPrefix = conf.aws.outputPrefix
     val dryRun = params.contains("--dryrun")
 
     val s3VCFFiles = CheckS3Data.ls(bucket, batchId)
 
-    s3VCFFiles.foreach(s3VCF => {
-      val aliquotIDs = extractAliquotIDs(bucket, s3VCF.key)
-      compareWithFileName(s3VCF.filename, aliquotIDs)
-      val (taskGermline, taskSomatic) = findFhirTasks(aliquotIDs)(fhirClient)
-      compareTasksPatient(taskGermline, taskSomatic)
-      val documentReference = buildDocumentReference()
-    })
+    var res: Seq[BundleEntryComponent] = Seq()
+    var files : Seq[FileEntry] = Seq()
+
+    s3VCFFiles
+      .filter(f => f.filename.endsWith(".vcf.gz"))  // keep only VCF files
+      .foreach(s3VCF => {
+        // find associated TBI (is it optional? could be ... let make it mandatory for now)
+        val s3VCFTbi = s3VCFFiles.find(f => f.filename.equals(s3VCF.filename+".tbi")).getOrElse(throw new IllegalStateException(s"Cant find TBI file for: ${s3VCF.filename}"))
+
+        val aliquotIDs = extractAliquotIDs(bucket, s3VCF.key)
+        LOGGER.info(s"${s3VCF.filename} contains aliquot IDs: ${aliquotIDs.mkString(" ")}")
+
+        val (taskGermline, taskSomatic) = findFhirTasks(aliquotIDs)(fhirClient)
+        validateTasks(s3VCF, taskGermline, taskSomatic)
+
+        val (copiedVCF, copiedTBI) = prepareCopy(bucketOutputPrefix, s3VCF, s3VCFTbi)
+        files = files ++ Seq(copiedVCF, copiedTBI)
+
+        val documentReference = buildDocumentReference(conf.ferload.cleanedUrl, taskGermline, taskSomatic, copiedVCF, copiedTBI)
+        val task = buildTask(taskGermline, taskSomatic)
+        res = res ++ FhirUtils.bundleCreate(Seq(documentReference, task))
+      })
+
+    val bundle = TBundle(res.toList)
+    LOGGER.info("Request:\n" + bundle.print()(fhirClient))
+    if (dryRun) {
+      Valid(true)
+    } else {
+      /*val result = bundle.save()(fhirClient)
+      LOGGER.info("Response :\n" + FhirUtils.toJson(result.toList.head)(fhirClient))
+      Valid(result.isValid)*/
+      Valid(true)
+    }
 
     /*
     TODO
@@ -68,19 +102,75 @@ object SomaticNormalImport extends App {
     */
   }
 
-  private def buildDocumentReference() = {
+  private def prepareCopy(prefix: String, vcf: RawFileEntry, tbi: RawFileEntry) = {
+    val uuid = UUID.randomUUID().toString
+    (FileEntry(vcf, s"$prefix/$uuid", None, APPLICATION_OCTET_STREAM.getMimeType, attach(vcf.filename)),
+      FileEntry(tbi, s"$prefix/$uuid.tbi", None, APPLICATION_OCTET_STREAM.getMimeType, attach(vcf.filename)))
+  }
+
+  private def buildDocumentReference(ferloadURL: String, taskGermline: Task, taskSomatic: Task, copiedVCF: FileEntry, copiedTBI: FileEntry) = {
+
+    def addContext(doc: DocumentReference, task: Task, displayType: String) = {
+      val specimen = task.getInputFirstRep.getValue.asInstanceOf[Reference]
+      doc.getContext.addRelated().setReference(specimen.getReference).setDisplay(s"Submitter $displayType Sample ID: ${specimen.getDisplay.split(":")(1).trim}")
+    }
+
+    def addContent(doc: DocumentReference, file: FileEntry, formatType: String) = {
+      val content = doc.addContent()
+      val attachment = new Attachment()
+      attachment.addExtension(FULL_SIZE, new DecimalType(file.size))
+      attachment.setContentType(APPLICATION_OCTET_STREAM.getMimeType)
+      attachment.setUrl(s"$ferloadURL/${file.id}")
+      attachment.setTitle(file.filename)
+      content.setAttachment(attachment)
+      val format = new Coding()
+      format.setSystem(DR_FORMAT)
+      format.setCode(formatType)
+      content.setFormat(format)
+    }
+
     val doc = new DocumentReference
     doc.setStatus(DocumentReferenceStatus.CURRENT)
-    doc.setType(new CodeableConcept(new Coding().setSystem("http://fhir.cqgc.ferlab.bio/CodeSystem/data-type").setCode("SSNV")))
+    doc.setType(new CodeableConcept(new Coding().setSystem(DR_TYPE).setCode("SSNV")))
+    doc.addCategory(new CodeableConcept(new Coding().setSystem(DR_CATEGORY).setCode("GEMO")))
+    doc.setSubject(new Reference(taskSomatic.getFor.getReference))
+
+    addContent(doc, copiedVCF, "VCF")
+    addContent(doc, copiedTBI, "TBI")
+
+    addContext(doc, taskGermline, "Normal")
+    addContext(doc, taskSomatic, "Tumor")
+
     doc
   }
 
-  private def compareTasksPatient(taskGermline: Task, taskSomatic: Task) = {
+  private def buildTask(taskGermline: Task, taskSomatic: Task) = {
+    val task = new Task()
+    task.setCode(new CodeableConcept(new Coding().setSystem(ANALYSIS_TYPE).setCode("TNEBA")))
+    task
+  }
+
+  private def validateTasks(s3VCF: RawFileEntry, taskGermline: Task, taskSomatic: Task) = {
+    val file = s3VCF.filename
+    val aliquotIDGermline = extractAliquotID(taskGermline).get
+    val aliquotIDSomatic = extractAliquotID(taskSomatic).get
+
+    if (!file.startsWith(s"T-$aliquotIDSomatic.N-$aliquotIDGermline")) {
+      throw new IllegalStateException(s"$file file name doesn't match aliquot IDs inside")
+    }
+
     val patientGermline = taskGermline.getFor.getReference
     val patientSomatic = taskSomatic.getFor.getReference
     if (!patientGermline.equals(patientSomatic)) {
       throw new IllegalStateException(s"Tasks don't have the same patient for reference germline => $patientGermline and somatic => $patientSomatic")
     }
+  }
+
+  private def extractAliquotID(task : Task) = {
+    Some(task.getExtensionByUrl(SEQUENCING_EXPERIMENT))
+      .map(_.getExtensionByUrl("labAliquotId"))
+      .map(_.getValue)
+      .map(_.toString)
   }
 
   private def findFhirTasks(aliquotIDs: Array[String])(fhirClient: IGenericClient) = {
@@ -91,12 +181,7 @@ object SomaticNormalImport extends App {
     }
 
     def hasAnyAliquotIds(task: Task, aliquotIDs: Array[String]) = {
-      val aliquot = Some(task.getExtensionByUrl("http://fhir.cqgc.ferlab.bio/StructureDefinition/sequencing-experiment"))
-        .map(_.getExtensionByUrl("labAliquotId"))
-        .map(_.getValue)
-        .map(_.toString)
-        .orNull
-      Some(aliquot).filter(aliquotIDs.contains)
+      extractAliquotID(task).filter(aliquotIDs.contains)
     }
 
     var taskGermline: Option[Task] = None
@@ -127,17 +212,6 @@ object SomaticNormalImport extends App {
     }
 
     (taskGermline.get, taskSomatic.get)
-  }
-
-  private def compareWithFileName(file: String, aliquotIDs: Array[String]): Unit = {
-    // we don't mind the aliquots order
-    def validFileName(somaticAliquot: String, germlineAliquot: String): Boolean = {
-      file.startsWith(s"T-$somaticAliquot.N-$germlineAliquot")
-    }
-    LOGGER.info(s"$file contains aliquot IDs: ${aliquotIDs.mkString(" ")}")
-    if (!validFileName(aliquotIDs(0), aliquotIDs(1)) && !validFileName(aliquotIDs(1), aliquotIDs(0))) {
-      throw new IllegalStateException(s"$file file name doesn't match aliquot IDs inside")
-    }
   }
 
   private def extractAliquotIDs(bucket: String, key: String)(implicit s3Client: S3Client) = {
